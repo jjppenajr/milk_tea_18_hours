8aa0e52fd38c22ec51fe70ff3fff120d82235e60
Jayjay Pena <jjppenajr@gmail.com>
2020-05-17 03:09:14 +0800

    Add arctic admin.

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'devise'
 #active_admin
 gem 'activeadmin'
 gem 'activeadmin_addons'
+gem 'arctic_admin'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -332,4 +332,9 @@ ActiveAdmin.setup do |config|
   # You can switch to using Webpacker here.
   #
   # config.use_webpacker = true
+
+  #arctic admin
+  meta_tags_options = { viewport: 'width=device-width, initial-scale=1' }
+  config.meta_tags = meta_tags_options
+  config.meta_tags_for_logged_out_pages = meta_tags_options
 end
